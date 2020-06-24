1ef163379ec509db0758bef1729f4404f856f351
yaccscc <yaccscc@gmail.com>
2020-06-24 18:54:28 +0500

    redo add to basket test for multiple links

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,25 @@
 from typing import Union
 import time
 
+import pytest
 from selenium import webdriver
 
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser: Union[webdriver.Chrome, webdriver.Firefox]):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param(
+                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                      marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser: Union[webdriver.Chrome, webdriver.Firefox], link):
     page = ProductPage(link, browser)
     page.open()
     page.add_to_basket()
