cae3efcf968de38ca0e71a34186b684d28bb707a
yaccscc <yaccscc@gmail.com>
2020-06-24 19:41:02 +0500

    added is_element_present in BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,24 @@
 from typing import Union
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 
 class BasePage:
-    def __init__(self, url: str, browser: Union[webdriver.Chrome, webdriver.Firefox]):
+    def __init__(self, url: str, browser: Union[webdriver.Chrome, webdriver.Firefox], timeout: int = 10):
         self.url = url
         self.browser = browser
+        self.timeout = timeout
+
+        self.browser.implicitly_wait(self.timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how: By, what: str):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,12 @@
 from .pages.main_page import MainPage
 
 
+def test_quest_should_see_login_link(browser):
+    page = MainPage("http://selenium1py.pythonanywhere.com/", browser)
+    page.open()
+    page.should_be_login_link()
+
+
 def test_guest_can_go_to_login_page(browser):
     page = MainPage("http://selenium1py.pythonanywhere.com/", browser)
     page.open()
